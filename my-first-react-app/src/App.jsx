4aea9fab2c64d0ed263a24459178a8992a79475c
Kaveenxz <kaveenhansithx@gmail.com>
2025-03-08 12:55:11 +0530

    designed base ui

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from './components/Search'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState()
   return (
     <main>
       <div className='pattern'/>
@@ -12,7 +13,8 @@ const App = () => {
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy</h1>
         </header>
 
-        <Search/>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <h1>{searchTerm}</h1>
       </div>
     </main>
   )
--- my-first-react-app/src/components/Search.jsx
+++ my-first-react-app/src/components/Search.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
-const Search = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Search = ({ searchTerm, setSearchTerm }) => {
+    console.log(searchTerm)
+    return (
+        <div className='search'>
+            <div>
+                <img src="Vector.svg" alt="search" />
+                <input
+                    type="text"
+                    placeholder='Search through thousends of movies'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+        </div>
+    )
 }
 
 export default Search
